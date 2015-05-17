e153d21839d2b7d7109a016660131313f6bd5634
shosmer <shannon.hosmer@gmail.com>
2015-05-17 14:02:00 -0400

    Added javascript

---
+++ main.js
@@ -0,0 +1,5 @@
+var section1 = document.getElementById("section1");
+var button1 = document.getElementById("button1");
+
+var section2 = document.getElementById("section2");
+var button2 = document.getElementById("button2");
